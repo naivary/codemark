043f51956b4683cde113f18480cd2e4a6d0e3e37
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2024-09-20 17:14:14 +0200

    fmt.Println still from debugging left...

--- converter.go
+++ converter.go
@@ -37,7 +37,6 @@ func (c *converter) Convert(marker marker.Marker, target Target) (any, error) {
 		return nil, fmt.Errorf("marker `%s` is not defined in the registry", idn)
 	}
 	if target != def.Target {
-		fmt.Println("i am herre")
 		return nil, fmt.Errorf("marker `%s` is appliable to `%s`. Was applied to `%s`", idn, def.Target, target)
 	}
 	switch marker.Kind() {
