1c3152df113ed292859d821edf97e87fc3e1fdf0
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-06-09 01:23:33 +0200

    tommorow starting the work on list-conveter

--- converter_list.go
+++ converter_list.go
@@ -1,7 +1,6 @@
 package codemark
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 
@@ -12,16 +11,8 @@ var _ Converter = (*listConverter)(nil)
 
 type listConverter struct{}
 
-func (i *listConverter) SupportedTypes() []reflect.Type {
-	types := []any{
-		[]string{},
-		[]int{},
-		[]uint{},
-		[]complex64{},
-		[]complex128{},
-		[]float32{},
-		[]float64{},
-	}
+func (s *listConverter) SupportedTypes() []reflect.Type {
+	types := []any{}
 	supported := make([]reflect.Type, 0, len(types))
 	for _, typ := range types {
 		rtype := reflect.TypeOf(typ)
@@ -30,18 +21,16 @@ func (i *listConverter) SupportedTypes() []reflect.Type {
 	return supported
 }
 
-func (i *listConverter) CanConvert(m parser.Marker, def *Definition) error {
+func (l *listConverter) CanConvert(m parser.Marker, def *Definition) error {
 	if m.Kind() != parser.MarkerKindList {
-		return fmt.Errorf("marker kind of `%s` cannot be converted to a int. valid option is: %s\n", m.Kind(), parser.MarkerKindInt)
+		return fmt.Errorf("marker kind of `%s` cannot be converted to a string. valid option is: %s\n", m.Kind(), parser.MarkerKindString)
 	}
-	// NOTE: dont need to check if the def.output is supported because the converter
-	// will only be choosen if def.output is one of the supported types
 	return nil
 }
 
-func (i *listConverter) Convert(m parser.Marker, def *Definition) (any, error) {
+func (l *listConverter) Convert(m parser.Marker, def *Definition) (any, error) {
 	typeID := TypeID(def.output)
 	switch typeID {
 	}
-	return nil, errors.New("conversion was not possible")
+	return nil, fmt.Errorf("conversion of `%s` to `%s` is not possible", m.Ident(), def.output)
 }
