0b1be6568e168b9208a6b76904965dc39f714302
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-06-17 23:47:22 +0200

    this separation should work

---
+++ lexer/token.go
@@ -0,0 +1,29 @@
+package lexer
+
+import (
+	"fmt"
+
+	"github.com/naivary/codemark/lexer/token"
+)
+
+func NewToken(kind token.Kind, v string) Token {
+	return Token{
+		Kind:  kind,
+		Value: v,
+	}
+}
+
+type Token struct {
+	Kind  token.Kind
+	Value string
+}
+
+func (t Token) String() string {
+	switch t.Kind {
+	case token.EOF:
+		return "EOF"
+	case token.ERROR:
+		return t.Value
+	}
+	return fmt.Sprintf("%q", t.Value)
+}
---
+++ lexer/token/token.go
@@ -0,0 +1,18 @@
+package token
+
+type Kind int
+
+const (
+	EOF Kind = iota + 1
+	ERROR
+	STRING  // "codemark" (without the quotation marks)
+	BOOL    // true or false
+	IDENT   // codemark:valid:ident
+	ASSIGN  // =
+	PLUS    // +
+	LBRACK  // [
+	RBRACK  // ]
+	INT     // 1237123, 0x283f etc.
+	FLOAT   // 1.2
+	COMPLEX // 3 + 2i
+)
